4e8445a3e9816be297639bf1a05d882ed46c730e
Guanzhong Wang <cnhajzwgz@gmail.com>
2020-03-12 00:35:00 +0100

    made batch reshaping easier

--- transport_analysis.py
+++ transport_analysis.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def guess_2D_dims(inner_axis, outer_axis, data_2d):
+def guess_2D_dims(inner_axis, outer_axis, data_2d, rescale_xy = True):
     '''
     Takes X, Y and Z from load_by_id() as inputs.
     Returns reshaped X, Y, Z and the dimensions guessed.
@@ -12,10 +12,23 @@ def guess_2D_dims(inner_axis, outer_axis, data_2d):
     dims_guess[0] = int(np.floor(len(outer_axis) / dims_guess[1]))
     total_num = dims_guess[1] * dims_guess[0] # for incomplete sweeps where the unifinished row is discarded
 
-    return inner_axis[:total_num].reshape(dims_guess)[0,:], \
+    if rescale_xy:
+        return inner_axis[:total_num].reshape(dims_guess)[0,:], \
                          outer_axis[:total_num].reshape(dims_guess)[:,0], \
                          data_2d[:total_num].reshape(dims_guess), \
                          dims_guess
+    else:
+        return data_2d[:total_num].reshape(dims_guess), dims_guess
+
+def batch_guess_2D_dims(inner_axis, outer_axis, data_2d_list, rescale_xy = True):
+    new_dlist = []
+    for d in data_2d_list:
+        new_d, dims = guess_2D_dims(inner_axis, outer_axis, d, rescale_xy=False)
+        new_dlist.append(new_d)
+    
+    new_inner_axis, new_outer_axis, _, dims = guess_2D_dims(inner_axis, outer_axis, d, rescale_xy=True)
+
+    return new_inner_axis, new_outer_axis, new_dlist, dims
 
 
 # to add: bias correciton
