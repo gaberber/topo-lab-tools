0e138589a87cbd7b968ca6a26c667fa1e94ac5ad
David <david@vdriel.nl>
2020-08-07 16:47:35 +0200

    working BTK fitting

--- topo_lab_tools/gap_analysis.py
+++ topo_lab_tools/gap_analysis.py
@@ -1,11 +1,11 @@
 k= 8.617e-5 #eV/K Boltzmann
-T= 55e-3 #temp of 1 mK
+T= 30e-3 #temp of 1 mK
+c=1
 import numpy as np
 from scipy.optimize import curve_fit
 
 def gap(V, delta, Z, broadness, offset=0):
-    def ingap(E, delta):
-        return E <= delta
+    E = np.arange(1.5*min(V), 1.5*max(V), 1.5*k*T)
 
     def u2(E, delta): #u0 squared, BCS coefficient
         return .5 * (1+np.sqrt((E**2 - delta**2)/E**2))
@@ -16,44 +16,40 @@ def gap(V, delta, Z, broadness, offset=0):
     def gamma2(E, delta, Z): # gamma squared
         return (u2(E, delta) + (Z**2)*(u2(E, delta) - v2(E, delta)))**2
 
-    def A(E, delta, Z):
+    def A(E, delta, Z):  #Andreev reflection
         if E <= delta:
             return delta**2/(E**2 + (delta**2 - E**2)*(1+2*Z**2)**2)
         if E > delta:
             return (u2(E, delta)*v2(E, delta))/gamma2(E, delta, Z)
 
-    def B(E, delta, Z):
+    def B(E, delta, Z): #Ordinary reflection
         if E < delta:
             return 1 - delta**2/(E**2 + (delta**2 - E**2)*(1+2*Z**2)**2)
         if E >= delta:
             return ((u2(E, delta)-v2(E, delta))**2*Z**2*(1+Z**2))/gamma2(E, delta, Z)
 
-    def f0_deriv(expo):
-        return ((c/(k*T))*expo)/(expo+1)**2
-
-    def f0(expo):
-        return 1/(expo+1)
+    def f0_deriv(e, v):
+        A = (e-c*v)/(k*T)
+        #print(A)
+        #A = np.sign(A)* min(20, abs(A))
+        #print(type(A))
+        A[abs(A)>20] = 20
+        _f0_deriv = (c/(2*k*T))/(1+np.cosh(A))
+        return _f0_deriv
 
     def per_e(e, v, delta, Z, broadness):
-        expo_f = exp(v)
-        expo_f_deriv = exp(e-c*v)
-        return (f0_deriv(expo_f_deriv)-f0(expo_f))*(1 + A(e, delta, Z)-B(e, delta, Z))
-
-    def exp(E):
-        return np.exp(np.real(E)/(k*T))
+        _f0_deriv = f0_deriv(e, v)
+        return _f0_deriv*(1 + A(e+1j*broadness, delta, Z)-B(e+1j*broadness, delta, Z))
 
     def DIDV(V, delta, Z, broadness):
-        return np.sum(np.array([np.real(per_e(e+1j*broadness, V, delta, Z, broadness)) for e in E])  /len(E))
+        return np.sum(np.array([abs(per_e(e, V, delta, Z, broadness)) for e in E])/len(E), axis=0)
 
-    #Rn = .5*norm(delta)
-    #return scale*(1/Rn)*np.array([DIDV(abs(v-offset)) for v in V]) data not well normalized
-    norm= 2/DIDV(0, delta, 0, 0)
-    return np.array([norm * DIDV(abs(v-offset), delta, Z, broadness) for v in V])
+    norm= 2/DIDV(np.array([[0]]), delta, 0, 0)
+    return (norm * DIDV(abs(V-offset), delta, Z, broadness)).reshape(len(V))
+    #return np.array([norm * DIDV(abs(v-offset), delta, Z, broadness) for v in V])
 
 def double_gap(V, delta1=2e-4, delta2=2e-4, Z1=1.0, Z2=1.0, broadness1 =1e-6, broadness2=1e-6, w=.5, offset=0):
-    def ingap(E, delta):
-        return E <= delta
-
+    E = np.arange(1.5*min(V), 1.5*max(V), 1.5*k*T)
     def u2(E, delta): #u0 squared, BCS coefficient
         return .5 * (1+np.sqrt((E**2 - delta**2)/E**2))
 
@@ -75,35 +71,32 @@ def double_gap(V, delta1=2e-4, delta2=2e-4, Z1=1.0, Z2=1.0, broadness1 =1e-6, br
         if E >= delta:
             return ((u2(E, delta)-v2(E, delta))**2*Z**2*(1+Z**2))/gamma2(E, delta, Z)
 
-    def f0_deriv(expo):
-        return ((c/(k*T))*expo)/(expo+1)**2
-
-    def f0(expo):
-        return 1/(expo+1)
+    def f0_deriv(e, v):
+        A = (e-c*v)/(k*T)
+        A = np.sign(A)* min(20, abs(A))
+        _f0_deriv = (c/(2*k*T))/(1+np.cosh(A))
+        return _f0_deriv
 
     def per_e(e, v, delta, Z, broadness):
-        expo_f = exp(v)
-        expo_f_deriv = exp(e-c*v)
-        return (f0_deriv(expo_f_deriv)-f0(expo_f))*(1 + A(e, delta, Z)-B(e, delta, Z))
-
-    def exp(E):
-        return np.exp(np.real(E)/(k*T))
+        _f0_deriv = f0_deriv(e, v)
+        return _f0_deriv*(1 + A(e+1j*broadness, delta, Z)-B(e+1j*broadness, delta, Z))
 
     def DIDV(V, delta, Z, broadness):
-        return np.sum(np.array([np.real(per_e(e+1j*broadness, V, delta, Z, broadness)) for e in E])  /len(E))
+        return np.sum(np.array([abs(per_e(e, V, delta, Z, broadness)) for e in E])/len(E))
 
-    #Rn = .5*norm(delta)
-    #return scale*(1/Rn)*np.array([DIDV(abs(v-offset)) for v in V]) data not well normalized
-    norm1, norm2 = 2/DIDV(0, delta1, 0, 0), 2/DIDV(0, delta2, 0, 0)
-    return np.array([w * norm1 *DIDV(abs(v-offset), delta1, Z1, broadness1) +
-                     (1-w) * norm2 *DIDV(abs(v-offset), delta2, Z2, broadness2) for v in V])
+    norm1, norm2 = 2/DIDV(np.array([[0]]), delta1, 0, 0), 2/DIDV(np.array([[0]]), delta2, 0, 0)
+    return (w * norm1 *DIDV(abs(V-offset), delta1, Z1, broadness1) +
+                     (1-w) * norm2 *DIDV(abs(V-offset), delta2, Z2, broadness2) ).reshape(len(V))
 
 def fit_gap(Vn, 
            gn, 
-           p0=[2e-4,1.0,1e-6,0],
+        delta=2.25e-4, 
+            Z=2, 
+            broadness=1e-6, 
+            offset=0,
            bounds = (
-            (2.4e-4,0,0, -.1),
-            (2.7e-4,2,1e-4, .1)
+            (1e-5,0,0, -.1),
+            (2.7e-3,4,1e-4, .1)
             )):
     """
     Fits a NS linecurve
@@ -111,20 +104,26 @@ def fit_gap(Vn,
     Arguments:
         Vn: Vbias range in volts
         gn: conductance data in (single) conductance quanta
-        p0: ansatz fitting parameters like: [delta1, delta2, Z1, Z2, broadness1, broadness2, w, offset]
-        delta is the gap, Z is the barrier parameter, broadness the Dynes parameter, w weight factor of both gaps, offset is zero bias offset
+        ansatz fitting parameters like: delta, Z, broadening, offset
+        delta is the gap, Z is the barrier parameter, broadness the Dynes parameter 
         bounds: tuple of bounds for fitting parameters
+    Returns:
+        g_fit
+        popt
+        pcov
     """
+    p0=delta, Z, broadness, offset
     popt, pcov = curve_fit(gap, Vn, gn, p0 = p0, bounds = bounds)
-    g_fit = gap(Vn, **popt)
+    delta, Z, broadness, offset = popt
+    g_fit = gap(Vn, delta, Z, broadness, offset)
     return g_fit, popt, pcov
 
 def fit_double_gap(Vn, 
                    gn, 
                    p0=[2e-4,2e-4, 1.0,1.0, 1e-6, 1e-6, .5, 0],
                    bounds = (
-                    (2.4e-4,2e-4,0,0,0,0,0, -.1),
-                    (2.7e-4,2.4e-4,2,2,1e-4,1e-4,1, .1)
+                    (1e-3, 1e-3 ,0 , 0, 0, 0, 0, -.1),
+                    (2.7e-3, 2.7e-3, 4, 4, 1e-4, 1e-4, 1, .1)
                     )):
     """
     Fits a linecurve that shows two seperate gaps
@@ -132,9 +131,13 @@ def fit_double_gap(Vn,
     Arguments:
         Vn: Vbias range in volts
         gn: conductance data in (single) conductance quanta
-        p0: ansatz fitting parameters like: [delta1, delta2, Z1, Z2, broadness1, broadness2, w, offset]
+        ansatz fitting parameters like: delta1, delta2, Z1, Z2, broadness1, broadness2, w, offset
         delta is the gap, Z is the barrier parameter, broadness the Dynes parameter, w weight factor of both gaps, offset is zero bias offset
         bounds: tuple of bounds for fitting parameters
+    Returns:
+        g_fit
+        popt
+        pcov
     """
     popt, pcov = curve_fit(double_gap, Vn, gn, p0 = p0, bounds = bounds)
     g_fit = two_gap(Vn, **popt)
--- topo_lab_tools/transport_analysis.py
+++ topo_lab_tools/transport_analysis.py
@@ -2,7 +2,7 @@ import os, re
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-
+from qcodes import load_by_run_spec
 def guess_2D_dims(inner_axis, outer_axis, data_2d, rescale_xy = True):
     '''
     Takes X, Y and Z from load_by_id() as inputs.
@@ -107,4 +107,101 @@ class Dataset_2d_spyview(Dataset_2d):
         col_labels = [metadata[k].strip() for k in range(13, len(metadata), 2)]
         
         Dataset_2d.create_labels(self, metadata[3].strip(), metadata[7].strip(), col_labels)
+  
+class Dataset_qcodes(Dataset_2d):
+    def __init__(self, run_id):
+        """
+        Reads a qcodes database.
+        """
+        self.dataset = load_by_run_spec(captured_run_id=run_id)
+        self.id = run_id
+        self.exp_name = self.dataset.exp_name
+        self.sample_name = self.dataset.sample_name
+        self.data = self.dataset.get_data_as_pandas_dataframe()
+        self.data_labels = self.data.keys()
+        self.instruments = self.dataset.snapshot['station']["instruments"].keys()
+        self.rename_instruments()
+        self.line_restist_sub = False
+        
+    def diff_cond(self, lockin=1, ac_scaling = 1):
+        #G = self.data["dIdV {}".format(i)]
+        if not self.line_restist_sub:
+            print("Beware, line resistance is not subtracted yet")
+        dI = self.data["lockin {}".format(lockin)]
+        dV = ac_scaling * self.lockins_ac["lockin {}".format(lockin)]
+        self.G = 12906*dI.values.reshape(len(dI))/dV.reshape(len(dI))
+        
+                
+    def subt_line_resist(self, I_measure_col, lineR=8484):
+        assert I_measure_col in self.data_labels, "column doesn't exist, choose from: {}".format(self.data_labels)
+        assert not self.line_restist_sub, "Line resist already subtracted"
+        I_measure = self.data[I_measure_col].values
+        I_measure = I_measure.reshape(len(I_measure))
+        V_bias = self.data[I_measure_col].index.values
+        V_bias = V_bias.reshape(len(I_measure))
+        R = V_bias/I_measure
+        self.V_new = V_bias * (R/(R + lineR))
+        for k,v in self.lockins_ac.items():
+            self.lockins_ac[k] = self.lockins_ac[k] * (R/(R + lineR))
+        
+        self.line_restist_sub = True
+            
+    def rename_instruments(self):
+        self.lockins_ac= {}
+        i = 0
+        for instrument in self.instruments:
+            a = re.search("lockin", instrument)
+            if a:
+                i += 1
+                
+                V_ac = self.dataset.snapshot['station']["instruments"][instrument]['parameters']['amplitude']['value']
+                self.lockins_ac["lockin {}".format(i)] = V_ac
+                _d = self.dataset.snapshot['station']["instruments"][instrument] 
+                #del self.dataset.snapshot['station']["instruments"][instrument]
+                #print(self.dataset.snapshot['station']["instruments"].keys())
+                self.dataset.snapshot['station']["instruments"]["lockin {}".format(i)]=_d
+                
+        self.keithleys= 0
+        for instrument in self.instruments:
+            a = re.search("keithley", instrument)
+            if a:
+                self.keithleys += 1
+        i=1   
+        cols = self.data.keys()
+        for col in cols:
+            a = re.search("keithley", col)
+            if a:
+                _d = self.data[col] 
+                del self.data[col]
+                self.data["keithley {}".format(i)] = _d
+                i+=1       
+        i=1
+        for col in cols:
+            a = re.search("K", col)
+            if a:
+                _d = self.data[col] 
+                del self.data[col]
+                self.data["keithley {}".format(i)] = _d
+                i+=1
+        i=1
+        for col in cols:
+            a = re.search("lockin", col)
+            if a:
+                _d = self.data[col] 
+                del self.data[col]
+                self.data["lockin {}".format(i)] = _d
+                #self.data["dIdV {}".format(i)] = _d
+                i+=1
+        for col in cols:
+            a = re.search("L", col)
+            if a:
+                _d = self.data[col] 
+                del self.data[col]
+                self.data["lockin {}".format(i)] = _d
+                #self.data["dIdV {}".format(i)] = _d
+                i+=1
+        self.data_labels = self.data.keys()
+        self.instruments = self.dataset.snapshot['station']["instruments"].keys()
+
+            
         
