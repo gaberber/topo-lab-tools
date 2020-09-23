e9f1c8b30e39b2bf23e020044254d3c3c592d81f
Guanzhong Wang <cnhajzwgz@gmail.com>
2020-09-24 00:36:38 +0200

    added linecut viewer

--- topo_lab_tools/interactive_plot.py
+++ topo_lab_tools/interactive_plot.py
@@ -55,3 +55,51 @@ def plot_colormap_interactive(x, y, z, xlabel=None, ylabel=None, zlabel=None, ad
     buttons_colorscheme.on_clicked(_colorfunc)
 
     return fig, ax, heat_map
+
+
+# to do: draw and label things better
+def plot_linecut_viewers(fig_2d, ax_2d, mesh_2d, x, y, z, marker_color='white'):
+    """
+    Takes the fig, ax, mesh of a heatmap and creates interactive x and y linecut viewers.
+    x, y, z need to be supplied and thus are decoupled from actual 2d heatmap data,
+    so make sure they agree!
+    x, y are vectors and z is a matrix.
+    Returns the fig and axes created here.
+    """
+    fig_1d, axes_1d = plt.subplots(2, 1, figsize=(6,6))
+    plt.subplots_adjust(bottom=0.2)
+
+    hline = ax_2d.axhline(y=y[0], color=marker_color)
+    vline = ax_2d.axvline(x=x[0], color=marker_color)
+
+    line_xcut, = axes_1d[0].plot(x, z[0, :])
+    axes_1d[0].set_xlabel('x')
+    line_ycut, = axes_1d[1].plot(y, z[:, 0])
+    axes_1d[1].set_xlabel('y')
+
+    ax_yslider = plt.axes([0.2, 0.1, 0.7, 0.03])
+    ax_xslider = plt.axes([0.2, 0.05, 0.7, 0.03])
+    slider_y = Slider(ax_yslider, 'y index', 0, len(y), valinit=0, valstep=1)
+    slider_x = Slider(ax_xslider, 'x index', 0, len(x), valinit=0, valstep=1)
+
+    def _update(val):
+        x_ind = slider_x.val
+        y_ind = slider_y.val
+        data_xcut = z[int(y_ind), :]
+        data_ycut = z[:, int(x_ind)]
+
+        line_xcut.set_data(x, data_xcut)
+        x_span = data_xcut.max() - data_xcut.min()
+        axes_1d[0].set_ylim((data_xcut.min() - x_span*0.05, data_xcut.max() + x_span * 0.05))
+        line_ycut.set_data(y, data_ycut)
+        y_span = data_ycut.max() - data_ycut.min()
+        axes_1d[1].set_ylim((data_ycut.min() - y_span*0.05, data_ycut.max() + y_span * 0.05))
+
+        hline.set_ydata([y[int(y_ind)], y[int(y_ind)]])
+        vline.set_xdata([x[int(x_ind)], x[int(x_ind)]])
+    #     fig.canvas.draw_idle()
+
+    slider_x.on_changed(_update)
+    slider_y.on_changed(_update)
+
+    return fig_1d, axes_1d
